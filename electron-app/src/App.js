935034a9e4a7da55e70b853d16dd8c49935affe7
Filip Åhfelt <filahf94@gmail.com>
2020-06-05 18:15:55 +0200

    dep update

--- electron-app/src/App.js
+++ electron-app/src/App.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import socketIOClient from "socket.io-client";
 
-import './App.css';
+import "./App.css";
 //const { ipcRender } = window.require('electron');
+const ENDPOINT = "localhost:5000";
 
 function App() {
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+    socket.on("connect", function (data) {
+      socket.emit("join", "Hello World from client");
+    });
+  }, []);
   return (
-    <div className='App'>
-      <header className='App-header'>
+    <div className="App">
+      <header className="App-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
         >
           Learn React
         </a>
