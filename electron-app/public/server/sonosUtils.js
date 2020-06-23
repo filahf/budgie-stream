8b975f0ae13ddbfffc74f05c4c377d031696ff25
Filip Åhfelt <filahf94@gmail.com>
2020-06-23 14:24:37 +0200

    server cleanup

--- electron-app/public/server/sonosUtils.js
+++ electron-app/public/server/sonosUtils.js
@@ -1,7 +1,13 @@
 const Sonos = require('sonos');
 const { ipcMain } = require('electron');
-
 const discovery = new Sonos.AsyncDeviceDiscovery();
+//var device = new Sonos('192.168.0.133');
+var ip = require('ip');
+const Store = require('electron-store');
+
+// Manage store
+const store = new Store();
+store.set('ip', ip.address());
 
 async function fetchDevices() {
   return discovery
@@ -22,3 +28,15 @@ ipcMain.on('fetchDevices', async (event, arg) => {
   console.log(devices);
   event.sender.send('devices', devices);
 });
+
+function play() {
+  device
+    .play('x-rincon-mp3radio://192.168.0.194:5000/stream.mp3')
+    .then((success) => {
+      console.log('Yeay');
+    })
+    .catch((err) => {
+      console.log('Error occurred %j', err);
+    });
+}
+//play();
