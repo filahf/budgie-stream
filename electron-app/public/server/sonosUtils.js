b4504547dd2b6e24792f7e738325175407ee144f
Filip Åhfelt <filahf94@gmail.com>
2020-07-11 13:31:46 +0200

    playback for multiple devices

--- electron-app/public/server/sonosUtils.js
+++ electron-app/public/server/sonosUtils.js
@@ -23,7 +23,6 @@ async function fetchDevices() {
 
 ipcMain.on('fetchDevices', async (event, arg) => {
   var devices = await fetchDevices();
-  console.log(devices);
   event.sender.send('devices', devices);
 });
 
@@ -32,27 +31,51 @@ const url = 'x-rincon-mp3radio://' + ip.address() + ':5000/stream.mp3';
 const metaData = `<DIDL-Lite xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:upnp="urn:schemas-upnp-org:metadata-1-0/upnp/" xmlns:r="urn:schemas-rinconnetworks-com:metadata-1-0/" xmlns="urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/"><item id="R:0/0/49" parentID="R:0/0" restricted="true"><upnp:albumArtURI>https://filahf.github.io/budgie-stream-supporters/icon.png</upnp:albumArtURI><dc:title>Budgie Stream</dc:title></item></DIDL-Lite>`;
 
 // Toggle Playback
-function togglePlayback(startPlaying) {
-  if (startPlaying) {
-    groupsAvail[0]
-      .CoordinatorDevice()
-      .play({
-        uri: url,
-        metadata: metaData,
-      })
-      .then((success) => {
-        console.log('Yeay');
-      })
-      .catch((err) => {
-        console.log('Error occurred %j', err);
-      });
-  } else {
-    groupsAvail[0].CoordinatorDevice().stop();
-  }
+function togglePlayback(devices, startPlaying) {
+  groupsAvail.map((group) => {
+    if (devices.includes(group.Name)) {
+      if (startPlaying) {
+        group
+          .CoordinatorDevice()
+          .play({
+            uri:
+              'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-4.mp3',
+            metadata: metaData,
+          })
+          .then((success) => {
+            console.log('Yeay');
+          })
+          .catch((err) => {
+            console.log('Error occurred %j', err);
+          });
+      } else {
+        group.CoordinatorDevice().stop();
+      }
+    }
+  });
+
+  // if (startPlaying) {
+  //   groupsAvail[0]
+  //     .CoordinatorDevice()
+  //     .play({
+  //       uri: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-4.mp3',
+  //       metadata: metaData,
+  //     })
+  //     .then((success) => {
+  //       console.log('Yeay');
+  //     })
+  //     .catch((err) => {
+  //       console.log('Error occurred %j', err);
+  //     });
+  // } else {
+  //   groupsAvail[0].CoordinatorDevice().stop();
+  // }
 }
 
 ipcMain.on('togglePlayback', (event, arg) => {
-  togglePlayback(arg.startPlaying);
+  const { devices, startPlaying } = arg;
+  const deviceNames = devices.map((a) => a.name);
+  togglePlayback(deviceNames, startPlaying);
 });
 
 // Volume control
