d3e9a1ff4f7108ad29ceeca0e3d7a1e16a18d5da
Filip Åhfelt <filahf94@gmail.com>
2020-07-13 09:43:00 +0200

    eslint

--- electron-app/src/.eslintrc.js
+++ electron-app/src/.eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  extends: ['plugin:react/recommended', 'plugin:prettier/recommended'],
+  extends: ["react-app",'plugin:react/recommended', 'plugin:prettier/recommended'],
   plugins: [],
   // add your custom rules here
   rules: {
