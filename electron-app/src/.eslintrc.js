f88c6a826cb1732921766d51248a588383ee6ba8
Filip Åhfelt <filahf94@gmail.com>
2020-07-13 12:06:24 +0200

    linting

--- electron-app/src/.eslintrc.js
+++ electron-app/src/.eslintrc.js
@@ -4,13 +4,24 @@ module.exports = {
     browser: true,
     node: true,
   },
+  settings: {
+    "react": {
+      "pragma": "React",
+      "version": "detect"
+    }
+  },
   parserOptions: {
     parser: 'babel-eslint',
-    ecmaVersion: 2018,
+    ecmaVersion: 11,
     sourceType: 'module',
   },
-  extends: ["react-app",'plugin:react/recommended', 'plugin:prettier/recommended'],
-  plugins: [],
+  extends:  [
+    "prettier",
+    "prettier/standard",
+    "prettier/react",
+    "plugin:react/recommended"
+  ],
+  plugins: ["prettier"],
   // add your custom rules here
   rules: {
     'react/prop-types': 1,
@@ -26,4 +37,5 @@ module.exports = {
         }
       ]
   },
+  
 };
